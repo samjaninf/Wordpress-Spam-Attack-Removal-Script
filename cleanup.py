b4ce91876e591b591adae978d97e003346539a64
Jazzy Jones <samjaninf@users.noreply.github.com>
2022-05-16 15:30:54 -0400

    ignore images and add newline to log

--- cleanup.py
+++ cleanup.py
@@ -10,12 +10,16 @@ logs = open("cleanup.log","w")
 for dname, dirs, files in os.walk(sys.argv[1]):
     for fname in files:
         fpath = os.path.join(dname, fname)
-        with open(fpath) as f:
-            s = f.read()
+        with open(fpath, encoding='utf-8', errors='ignore') as f:
+            print(fpath)
+            if fname.lower().endswith((".png", ".jpg", ".jpeg", ".ico", ".svg")):
+                continue
+            else:
+                s = f.read()
         length_of_original = len(s)
         for record in malware_records:
             s = s.replace(record['string'], replacement)
         if len(s) != length_of_original :
-            logs.write("Cleaning " + fname + " in " + dname)
+            logs.write("Cleaning " + fname + " in " + dname + "\n")
         with open(fpath, "w") as f:
             f.write(s)
